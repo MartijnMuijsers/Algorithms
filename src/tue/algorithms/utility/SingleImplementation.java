248070194b86fe2d2fa44a4828667030ca10bd93
Martijn Muijsers <martijnmuijsers@live.nl>
2014-04-24 15:10:40 +0200

    Added abstract implementations.

--- src/tue/algorithms/utility/SingleImplementation.java
+++ src/tue/algorithms/utility/SingleImplementation.java
@@ -10,7 +10,7 @@ public abstract class SingleImplementation {
 	
 	/**
 	 * Get the solution to a problem: given nodes as input, find the segments to be best connected,
-	 * where the segments should form a single curve.
+	 * where the segments should form a single curve, each open or closed.
 	 * @param input The nodes as input
 	 * @return The segments as output
 	 */
