c1647a8b0a9388aec6179d1d909a77cefdb82afe
Martijn Muijsers <martijnmuijsers@live.nl>
2014-04-25 11:37:06 +0200

    Made Pair class neat and added Overrides for Object methods.

--- src/tue/algorithms/other/Pair.java
+++ src/tue/algorithms/other/Pair.java
@@ -1,5 +1,6 @@
 package tue.algorithms.other;
 
+
 /**
  * A class to provide basic pair functionality.
  * @author Martijn
@@ -7,6 +8,8 @@ package tue.algorithms.other;
  * @param <B> The type of the second element of the pair.
  */
 public class Pair<A, B> {
+	
+	/* -- START Private final fields -- */
 	
 	/**
 	 * The first element of the pair.
@@ -16,6 +19,24 @@ public class Pair<A, B> {
 	 * The second element of the pair.
 	 */
 	private B second;
+	
+	/* -- END Private final fields -- */
+	
+	/* -- START Constructors -- */
+	
+	/**
+	 * Create a pair with two elements.
+	 * @param first The first element.
+	 * @param second The second element.
+	 */
+	public Pair(A first, B second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	/* -- END Constructors -- */
+	
+	/* -- START Public getters for private fields -- */
 	
 	/**
 	 * Get the first element of the pair.
@@ -33,14 +54,32 @@ public class Pair<A, B> {
 		return second;
 	}
 	
-	/**
-	 * Create a pair with two elements.
-	 * @param first The first element.
-	 * @param second The second element.
-	 */
-	public Pair(A first, B second) {
-		this.first = first;
-		this.second = second;
+	/* -- END Public getters for private fields -- */
+	
+	/* -- START Override equals(), hashCode() and toString() -- */
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Pair<?, ?>) {
+			Pair<?, ?> other = (Pair<?, ?>) obj;
+			return (other.first().equals(first()) && other.second().equals(second()));
+		}
+		return false;
 	}
+	
+	@Override
+	public int hashCode() {
+		return first().hashCode()+101+second().hashCode()*second().hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString()+"["
+				+"first="+first()+", "
+				+"second="+second()
+				+"]";
+	}
+	
+	/* -- END Override equals(), hashCode() and toString() -- */
 	
 }
