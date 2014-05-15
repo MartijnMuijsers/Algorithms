b352bb703b70b94be6bde1449a5f424ca814ee18
Rob Wu <rob@robwu.nl>
2014-05-15 17:12:57 +0200

    Refactor MST implementation
    Remove unnecessary copies, and move logic to a new namespace.

--- src/tue/algorithms/implementation/concrete/NetworkRMST.java
+++ src/tue/algorithms/implementation/concrete/NetworkRMST.java
@@ -2,12 +2,10 @@ package tue.algorithms.implementation.concrete;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
 import tue.algorithms.implementation.general.NetworkImplementation;
 import tue.algorithms.other.Pair;
+import tue.algorithms.utility.MinimumSpanningTree;
 import tue.algorithms.utility.Node;
 import tue.algorithms.utility.Point;
 import tue.algorithms.utility.Segment;
@@ -21,64 +19,24 @@ public class NetworkRMST extends NetworkImplementation {
 
     @Override
     public Pair<Segment[], Node[]> getOutput(Node[] input) {
-        HashSet<Segment> segments = new HashSet<Segment>();
-
-        for (Node node : input) {
-            for (Node node2 : input) {
-
-                if (!node.equals(node2)) {
-                    segments.add(new Segment(node, node2));
+        Segment[] segments = MinimumSpanningTree.getSegmentsPermutation(input);
+        Comparator<Segment> comparator = new Comparator<Segment>() {
+            @Override
+            public int compare(Segment s1, Segment s2) {
+                float diff = s1.manhattanDistance() - s2.manhattanDistance();
+                if (diff < 0) {
+                    return -1;
+                } else if (diff > 0) {
+                    return 1;
                 }
+                return 0;
             }
-        }
+        };
+                           
+        Segment[] result = MinimumSpanningTree.applyMST(segments, input, comparator);
+        result = Optimize(result, input);
+        return new Pair(result, null);
 
-        Segment[] resultSegments = new Segment[segments.size()];
-        int i = 0;
-        for (Segment segment : segments) {
-            resultSegments[i] = segment;
-            i++;
-        }
-
-        resultSegments = MSTsegments(resultSegments, input);
-        resultSegments = Optimize(resultSegments, input);
-        return new Pair(resultSegments, null);
-
-    }
-
-    public Segment[] MSTsegments(Segment[] segments, Node[] nodes) {
-        HashSet<Segment> mst = new HashSet<>();
-        Sort(segments);
-        HashMap<Node, Integer> A = new HashMap<Node, Integer>();
-        HashMap<Integer, HashSet<Node>> B = new HashMap<Integer, HashSet<Node>>();
-        for (Node node : nodes) {
-            A.put(node, node.getId());
-            HashSet<Node> set = new HashSet<Node>();
-            set.add(node);
-            B.put(node.getId(), set);
-        }
-        for (Segment segment : segments) {
-            int u = A.get(segment.getNode1());
-            int v = A.get(segment.getNode2());
-            if (u != v) {
-                mst.add(segment);
-                HashSet<Node> U = B.get(u);
-                HashSet<Node> V = B.get(v);
-                for (Node node : U) {
-                    A.put(node, v);
-                    V.add(node);
-                }
-                U.clear();
-            }
-        }
-
-        Segment[] resultSegments = new Segment[mst.size()];
-        int i = 0;
-        for (Segment segment : mst) {
-            resultSegments[i] = segment;
-            i++;
-        }
-
-        return resultSegments;
     }
 
     private Segment[] Optimize(Segment[] segments, Node[] nodes) {
@@ -114,39 +72,4 @@ public class NetworkRMST extends NetworkImplementation {
 
         return resultSegments;
     }
-
-    private void Sort(Segment[] segments) {
-
-        ArrayList<Segment> list = toArrayList(segments);
-
-        Collections.sort(list, segmentManhattanComparator);
-
-        for (int i = 0; i < segments.length; i++) {
-            segments[i] = list.get(i);
-        }
-
-    }
-
-    private Comparator<Segment> segmentManhattanComparator = new Comparator<Segment>() {
-
-        @Override
-        public int compare(Segment t, Segment t1) {
-            if (t.manhattanDistance() < t1.manhattanDistance()) {
-                return -1;
-            } else if (t.manhattanDistance() > t1.manhattanDistance()) {
-                return 1;
-            }
-            return 0;
-        }
-
-    };
-
-    private <T> ArrayList<T> toArrayList(T[] array) {
-        ArrayList<T> list = new ArrayList<T>();
-        for (T t : array) {
-            list.add(t);
-        }
-        return list;
-    }
-
 }
