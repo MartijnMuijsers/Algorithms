f9b4574eb2b8835df193a2d108d54477c16e37ab
wilcobrouwer <Polex123!>
2014-05-21 10:56:32 +0200

    Fixed the view (used to be upside down)

--- src/tue/algorithms/viewer/Simulation.java
+++ src/tue/algorithms/viewer/Simulation.java
@@ -179,7 +179,7 @@ public class Simulation {
         glColor3f(1f, 0f, 0f);
         if (brushMode){
             glPushMatrix();
-            glTranslatef((float) Mouse.getX() / Camera.width * 1.0f / Camera.SCALINGFACTOR - Camera.OFFSETFACTOR, 1 - ((float) Mouse.getY() / Camera.heigth * 1.0f / Camera.SCALINGFACTOR - Camera.OFFSETFACTOR), 0);
+            glTranslatef((float) Mouse.getX() / Camera.width * 1.0f / Camera.SCALINGFACTOR - Camera.OFFSETFACTOR, (float) Mouse.getY() / Camera.heigth * 1.0f / Camera.SCALINGFACTOR - Camera.OFFSETFACTOR, 0);
                         float pointSize = Math.min(Camera.width, Camera.heigth);
             glScalef(640/pointSize, 640/pointSize, 1);
             if (ratio > 1f) {
@@ -334,7 +334,7 @@ public class Simulation {
     
     private void deleteNodes() {
         float clickX = (float) Mouse.getX() / Camera.width * 1.0f / Camera.SCALINGFACTOR - Camera.OFFSETFACTOR;
-        float clickY = 1 - ((float) Mouse.getY() / Camera.heigth * 1.0f / Camera.SCALINGFACTOR - Camera.OFFSETFACTOR);
+        float clickY = (float) Mouse.getY() / Camera.heigth * 1.0f / Camera.SCALINGFACTOR - Camera.OFFSETFACTOR;
         Node mouseNode = new Node(0, clickX, clickY);
 
         for (Node node : nodes) {
@@ -355,7 +355,7 @@ public class Simulation {
 
     private void addNode() {
         float clickX = (float) Mouse.getX() / Camera.width * 1.0f / Camera.SCALINGFACTOR - Camera.OFFSETFACTOR;
-        float clickY = 1 - ((float) Mouse.getY() / Camera.heigth * 1.0f / Camera.SCALINGFACTOR - Camera.OFFSETFACTOR);
+        float clickY = (float) Mouse.getY() / Camera.heigth * 1.0f / Camera.SCALINGFACTOR - Camera.OFFSETFACTOR;
         if (clickX >= 0 && clickX <= 1 && clickY >= 0 && clickY <= 1) {
             boolean exists = false;
             for (Node node : nodes) {
